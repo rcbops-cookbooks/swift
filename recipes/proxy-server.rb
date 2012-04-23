36d18d6c14c62f2d21278341b568327f5642a665
William Kelly <the.william.kelly@gmail.com>
2012-04-23 12:02:28 -0500

    Renamed swift-* to *

---
+++ recipes/proxy-server.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: swift
+# Recipe:: swift-proxy-server
+#
+# Copyright 2012, Rackspace Hosting
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "swift::swift-common"
+include_recipe "swift::memcached"
+
+package "swift-proxy" do
+  action :upgrade
+  options "-o Dpkg::Options:='--force-confold' -o Dpkg::Options:='--force-confdef'"
+end
+
+package "python-swauth" do
+  action :upgrade
+  options "-o Dpkg::Options:='--force-confold' -o Dpkg::Options:='--force-confdef'"
+  only_if { node["swift"]["authmode"] == :swauth }
+end
+
+service "swift-proxy" do
+  supports :status => true, :restart => true
+  action :enable
+  only_if "[ -e /etc/swift/proxy-server.conf ] && [ -e /etc/swift/object.ring.gz ]"
+end
+
+template "/etc/swift/proxy-server.conf" do
+  source "proxy-server.conf.erb"
+  owner "swift"
+  group "swift"
+  mode "0600"
+  notifies :restart, resources(:service => "swift-proxy"), :immediately
+end
+
