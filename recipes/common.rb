5bbd333fd41bf99ba191212d1f789837e8e404e0
rpedde <ron@pedde.com>
2012-06-25 15:31:36 -0500

    make more prettier

--- recipes/common.rb
+++ recipes/common.rb
@@ -133,19 +133,10 @@ devices = (node["swift"]["state"]["devs"] || {}).inject([]) { |ary, (k,v)| ary <
 #           })
 # end
 
-
-########
-# These are a bunch of base monitors that should go...
-# in "base".  For right now, I'll hang them here
-
-%W{syslog cpu disk interface memory swap load}.each do |metric|
-  monitoring_metric metric do
-    type metric
-  end
-end
-
-
 # Sysctl tuning
 sysctl_multi "swift" do
-  instructions "net.ipv4.tcp_tw_reuse" => "1", "net.ipv4.ip_local_port_range" => "10000 61000", "net.ipv4.tcp_syncookies" => "0", "net.ipv4.tcp_fin_timeout" => "30"
+  instructions("net.ipv4.tcp_tw_reuse" => "1",
+               "net.ipv4.ip_local_port_range" => "10000 61000",
+               "net.ipv4.tcp_syncookies" => "0",
+               "net.ipv4.tcp_fin_timeout" => "30")
 end
