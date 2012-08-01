4085e570c5fa565c5b24a3d14a3a524134853b14
galstrom21 <jshepher@rackspace.com>
2012-08-01 10:50:52 -0500

    Correcting copyright banner

--- recipes/ring-repo.rb
+++ recipes/ring-repo.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: swift
 # Recipe:: ring-repo
 #
-# Copyright 2012, Rackspace Hosting
+# Copyright 2012, Rackspace US, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
