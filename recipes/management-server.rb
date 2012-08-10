0ac849ae629301074537dab008aac55b838b6e27
Joseph W. Breu <breu@breu.org>
2012-08-10 16:56:29 -0500

    Dispersion reporting in swift
    - added a dispersion user into the service tenant
    - patched swift-dispersion-report and swift-dispersion-populate to support v2
    - modified auto swift-dispersion-populate in the cookbook to only run if the dispersion_objects container does not exist.

--- recipes/management-server.rb
+++ recipes/management-server.rb
@@ -17,64 +17,88 @@
 # limitations under the License.
 #
 
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
 include_recipe "swift::common"
 include_recipe "monitoring"
 
-#include_recipe "swift::proxy-server"  # this is really only necessary for swauth.
+node.set_unless['swift']['dispersion_service_pass'] = secure_password
 
 # FIXME: This should probably be a role (ring-builder?), so you don't end up
 # with multiple repos!
 include_recipe "swift::ring-repo"
 
-platform_options = node["swift"]["platform"]
+# Apply hot patches for dispersion populate and report interop with keystone
+include_recipe "swift::swift-dispersion-patch"
 
-platform_options["swauth_packages"].each do |pkg|
-  package pkg do
-    action :upgrade
-    only_if { node["swift"]["authmode"] == "swauth" }
-  end
-end
+platform_options = node["swift"]["platform"]
 
 dsh_group "swift-storage" do
   admin_user "root"
   network "swift"
 end
 
+ks_admin_endpoint = get_access_endpoint("keystone", "keystone", "admin-api")
+ks_service_endpoint = get_access_endpoint("keystone", "keystone","service-api")
+keystone = get_settings_by_role("keystone", "keystone")
+keystone_auth_url = "http://#{ks_admin_endpoint["host"]}:#{ks_service_endpoint["port"]}/v2.0/"
+
+# Register Service Tenant
+keystone_register "Register Service Tenant" do
+  auth_host ks_admin_endpoint["host"]
+  auth_port ks_admin_endpoint["port"]
+  auth_protocol ks_admin_endpoint["scheme"]
+  api_ver ks_admin_endpoint["path"]
+  auth_token keystone["admin_token"]
+  tenant_name node["swift"]["service_tenant_name"]
+  tenant_description "Service Tenant"
+  tenant_enabled "true" # Not required as this is the default
+  action :create_tenant
+end
+
+# Register Service User
+keystone_register "Register Service User" do
+  auth_host ks_admin_endpoint["host"]
+  auth_port ks_admin_endpoint["port"]
+  auth_protocol ks_admin_endpoint["scheme"]
+  api_ver ks_admin_endpoint["path"]
+  auth_token keystone["admin_token"]
+  tenant_name node["swift"]["service_tenant_name"]
+  user_name node["swift"]["dispersion_service_user"]
+  user_pass node["swift"]["dispersion_service_pass"]
+  user_enabled "true" # Not required as this is the default
+  action :create_user
+end
+
+ ## Grant Admin role to Service User for Service Tenant ##
+  keystone_register "Grant 'admin' Role to Service User for Service Tenant" do
+    auth_host ks_admin_endpoint["host"]
+    auth_port ks_admin_endpoint["port"]
+    auth_protocol ks_admin_endpoint["scheme"]
+    api_ver ks_admin_endpoint["path"]
+    auth_token keystone["admin_token"]
+    tenant_name node["swift"]["service_tenant_name"]
+    user_name node["swift"]["dispersion_service_user"]
+    role_name node["swift"]["dispersion_service_role"]
+    action :grant_role
+  end
+
 # dispersion tools only work right now with swauth auth
 execute "populate-dispersion" do
   command "swift-dispersion-populate"
   user "swift"
   action :nothing
-  only_if { node["swift"]["authmode"] == "swauth" }
+  only_if "swift -V 2.0 -U #{node["swift"]["service_tenant_name"]}:#{node["swift"]["dispersion_service_user"]} -K '#{node["swift"]["dispersion_service_pass"]}' -A #{keystone_auth_url} stat dispersion_objects 2>&1 | grep 'Container.*not found'"
 end
 
-keystone = {}
-# FIXME: library this out
-if node["swift"]["authmode"] == "keystone"
-  result = node
-
-  if not Chef::Config[:solo]
-    (result,*_), _, _ = Chef::Search::Query.new.search(:node, "roles:keystone AND chef_environment:#{node.chef_environment}")
-    result = node if (result == nil or result.length <= 0)
-  end
-
-  keystone = result["keystone"].inject({}){ |hsh, (k,v)| hsh.merge(k => v) }
-
-  # FIXME: this should really return ip and port
-  keystone["api_ipaddress"]=IPManagement.get_access_ip_for_role("keystone", "swift-lb", node)
-  keystone["auth_url"] = "http://#{keystone['api_ipaddress']}:#{keystone['service_port']}/v2.0/"
-end
-
-# FIXME: broken for swauth
 template "/etc/swift/dispersion.conf" do
   source "dispersion.conf.erb"
   owner "swift"
   group "swift"
   mode "0600"
-  variables("auth_url" => keystone["auth_url"],
-            "auth_user" => keystone["admin_user"],
-            "auth_key" => keystone["users"][keystone["admin_user"]]["password"])
-
+  variables("auth_url" => keystone_auth_url,
+            "auth_user" => node["swift"]["dispersion_service_user"],
+            "auth_tenant" => node["swift"]["service_tenant_name"],
+            "auth_key" => node["swift"]["dispersion_service_pass"])
   only_if "swift-recon --objmd5 | grep -q '0 error'"
   notifies :run, "execute[populate-dispersion]", :immediately
 end
