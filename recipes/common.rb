f98ba2e4dd56fee009bc2de7cf339df14995a0dd
Joseph W. Breu <breu@breu.org>
2012-09-24 19:08:23 +0000

    fix foodcritic violation

--- recipes/common.rb
+++ recipes/common.rb
@@ -26,10 +26,6 @@ end
 platform_options = node["swift"]["platform"]
 git_service = get_access_endpoint("swift-management-server","swift","ring-repo")
 
-if platform?("centos","redhat","scientific")
-  include_recipe "yum::epel"
-end
-
 platform_options["swift_packages"].each do |pkg|
   package pkg do
     action :upgrade
