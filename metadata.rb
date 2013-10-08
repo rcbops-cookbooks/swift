840515367eb76a20403e5cfd5e7912fe3274b641
Joe Breu <breu@breu.org>
2013-10-08 17:54:55 +0000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures Openstack Swift"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.16"
+version           "4.2.0"
 recipe            "swift::account-server", "Installs the swift account server"
 recipe            "swift::object-server", "Installs the swift object server"
 recipe            "swift::proxy-server", "Installs the swift proxy server"
